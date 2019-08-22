aa177b0844d9d542fb0b010c9b20608aba4c387b
kanraz <kanraz@gmail.com>
2019-08-21 22:22:08 -0400

    this is my first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void nativeAppAndroid()
+	{
+		System.out.println("native android app");
+	}
+	@Test
+	public void IOSapps()
+	{
+		System.out.println("IOS Apps");
+	}
+
+}
